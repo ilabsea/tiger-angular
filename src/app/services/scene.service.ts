f129f087b3e0264dc9d0ea6b7d092e6a51b64331
Sokly Heng <hengsokly23@gmail.com>
2018-04-11 17:10:30 +0700

    handle authentication

--- src/app/services/scene.service.ts
+++ src/app/services/scene.service.ts
@@ -3,6 +3,12 @@ import { Scene } from '../models/scene';
 import { Observable } from 'rxjs/Observable';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Authorization': 'ZzA84aHAAeWtW9dhpfyn'
+  })
+};
+
 @Injectable()
 export class SceneService {
   API_URL: string = "http://192.168.1.107:3000/api/v1/";
@@ -12,21 +18,22 @@ export class SceneService {
   ) { }
 
   getScenes(story_id): Observable<Scene[]> {
-    return this.http.get<Scene[]>(this.API_URL + 'stories/' + story_id + '/scenes');
+    let endpoint = this.API_URL + 'stories/' + story_id + '/scenes';
+    return this.http.get<Scene[]>(endpoint, httpOptions);
   }
 
   createScene(story_id, body) {
     let endpoint = this.API_URL + 'stories/' + story_id + '/scenes';
-    return this.http.post(endpoint, body);
+    return this.http.post(endpoint, body, httpOptions);
   }
 
   updateScene(story_id, id, body) {
     let endpoint = this.API_URL + 'stories/' + story_id + '/scenes/' + id;
-    return this.http.put(endpoint, body);
+    return this.http.put(endpoint, body, httpOptions);
   }
 
   deleteScene(story_id, id) {
     let endpoint = this.API_URL + 'stories/' + story_id + '/scenes/' + id;
-    return this.http.delete(endpoint);
+    return this.http.delete(endpoint, httpOptions);
   }
 }

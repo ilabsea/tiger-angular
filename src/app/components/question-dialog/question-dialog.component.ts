173d5b493e0391438221d1bcdf1827e650dd75cf
Sokly Heng <hengsokly23@gmail.com>
2018-05-04 13:17:17 +0700

    enh(quiz): add quiz nested form dialog

--- src/app/components/question-dialog/question-dialog.component.ts
+++ src/app/components/question-dialog/question-dialog.component.ts
@@ -1,6 +1,9 @@
-import { Component, Inject } from '@angular/core';
+// * Nested Form
+// https://scotch.io/tutorials/how-to-build-nested-model-driven-forms-in-angular-2
+
+import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import { FormControl, Validators } from '@angular/forms';
+import { Validators, FormGroup, FormArray, FormBuilder } from '@angular/forms';
 import { QuestionService } from '../../services/question.service';
 
 @Component({
@@ -8,21 +11,84 @@ import { QuestionService } from '../../services/question.service';
   templateUrl: './question-dialog.component.html',
   styleUrls: ['./question-dialog.component.css']
 })
-export class QuestionDialogComponent {
-  label = new FormControl(this.data.label, [Validators.required]);
-  choices: any[] = [{label: 'Option1'}, {label: 'Option2'}, {label: 'Option3'}];
+export class QuestionDialogComponent implements OnInit {
+  myForm: FormGroup;
+  submmited: boolean = false;
+  archiveChoices: any = [];
+  count: number = 0;
 
   constructor(
     public dialogRef: MatDialogRef<QuestionDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private questionService: QuestionService
+    private questionService: QuestionService,
+    private _fb: FormBuilder,
   ) { }
 
+  ngOnInit() {
+    this.myForm = this._fb.group({
+      label: [this.data.label, [Validators.required]],
+      selectedChoice: ['', [Validators.required]],
+      choices: this._fb.array([])
+    });
+
+    if (!this.data.id) {
+      return this.addChoice();
+    }
+
+    this._handleSetChoice();
+  }
+
+  _handleSetChoice() {
+    for(let i=0; i<this.data.choices.length; i++) {
+      this.addChoice(this.data.choices[i]);
+    }
+
+    let index = this.myForm.value.choices.findIndex(item => item.answered == true);
+    this.myForm.controls.selectedChoice.setValue(this.myForm.value.choices[index]['_id']);
+  }
+
+  initChoice(obj={}) {
+    this.count++;
+
+    return this._fb.group({
+      _id: this.count,
+      id: [obj['id']],
+      label: [obj['label'], Validators.required],
+      answered: [obj['answered']]
+    });
+  }
+
+  addChoice(obj={}) {
+    const control = <FormArray>this.myForm.controls['choices'];
+    control.push(this.initChoice(obj));
+  }
+
+  removeChoice(i: number) {
+    this._handleSelectedChoice(i);
+    this._handleArchivedChoice(i);
+
+    const control = <FormArray>this.myForm.controls['choices'];
+    control.removeAt(i);
+  }
+
+  _handleSelectedChoice(i) {
+    if (this.myForm.value.selectedChoice == this.myForm.value.choices[i]['_id']) {
+      this.myForm.controls.selectedChoice.reset();
+    }
+  }
+
+  _handleArchivedChoice(i) {
+    if (!!this.myForm.value.choices[i].id) {
+      let obj = this.myForm.value.choices[i];
+      obj['_destroy'] = true;
+
+      this.archiveChoices.push(obj);
+    }
+  }
 
   handleSubmit(): void {
-    if (this.label.invalid) {
-      return;
-    }
+    this.submmited = true;
+    if (this.myForm.invalid) { return; }
 
     if (this.data.id) {
       return this._update();
@@ -56,9 +122,17 @@ export class QuestionDialogComponent {
   }
 
   _buildData() {
+    let choices = this.myForm.value.choices.slice();
+    let index = choices.findIndex(item => item['_id'] == this.myForm.value.selectedChoice);
+
+    choices.map(c => c['answered'] = null);
+    choices[index].answered = true;
+    choices = choices.concat(this.archiveChoices);
+
     let obj = {
       id: this.data.id,
-      label: this.label.value,
+      label: this.myForm.value.label,
+      choices_attributes: choices,
       story_id: this.data.story_id,
     }
 

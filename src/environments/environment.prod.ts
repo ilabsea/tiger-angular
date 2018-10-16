080c2aa3500d448d1367f5a001925dedce8afc2e
Sokly <hengsokly23@gmail.com>
2018-10-16 11:56:28 +0700

    fix url endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  endpointUrl: 'http://110.74.204.121:8282/app/',
+  endpointUrl: 'http://110.74.204.121:8282/app',
   apiUrl: 'http://110.74.204.121:8282/app/api/v1/'
 };

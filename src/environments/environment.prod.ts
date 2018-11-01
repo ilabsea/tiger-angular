867654334cae9f37df517f34d631ae14c1289870
Sokly <hengsokly23@gmail.com>
2018-11-01 11:38:56 +0700

    change environment production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  endpointUrl: 'http://110.74.204.121:8282/app',
-  apiUrl: 'http://110.74.204.121:8282/app/api/v1/'
+  endpointUrl: 'http://13.229.148.103/app',
+  apiUrl: 'http://13.229.148.103/app/api/v1/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  endpointUrl: 'http://192.168.1.141:3000',
-  apiUrl: 'http://192.168.1.141:3000/api/v1/'
+  endpointUrl: 'http://192.168.1.142:3000',
+  apiUrl: 'http://192.168.1.142:3000/api/v1/'
 };

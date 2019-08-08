8945927ef1f097c6eff9e09ee870da2fc907c238
borey <lim.borey@gmail.com>
2019-08-08 13:58:45 +0700

    setup docker for development

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  endpointUrl: 'http://192.168.1.110:3000',
-  apiUrl: 'http://192.168.1.110:3000/api/v1/'
+  endpointUrl: 'http://localhost:3000',
+  apiUrl: 'http://localhost:3000/api/v1/'
   // endpointUrl: 'https://tigerkh.org/app',
   // apiUrl: 'https://tigerkh.org/app/api/v1/'
 };
